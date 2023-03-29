76f9b51506a212a1e185dd5cfd69236090b85abb
Ömer <omergungorco@gmail.com>
2023-03-29 15:46:03 +0300

    Ejs, Routing & Partials are done.

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
+
 const port = 3000;
 
-app.get('/', (req, res) => {
-    const blog = {
-        id: 1,
-        title: 'My first blog',
-        description: 'This is my first blog'
-    }
+// TEMPLATE ENGINE
+// app.set('views', './');
+app.set('view engine', 'ejs');
 
-    res.send(blog);
+app.use(express.static('public'));
+
+//####### ROUTES #######
+
+app.get('/', (req, res) => {
+    res.render('index');
 });
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/add-new-post', (req, res) => {
+    res.render('add_post');
+});
+
+app.get('/blog/:id', (req, res) => {
+    res.render('post');
+});
+
+//####### ROUTES #######
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
